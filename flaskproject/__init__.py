599db0d24f2fb415196071a48a4953f3b0f6beb0
alonlerner <65507603+alonlerner@users.noreply.github.com>
2021-07-16 09:27:30 -0400

    added error pages-07-16-2021

--- flaskproject/__init__.py
+++ flaskproject/__init__.py
@@ -10,7 +10,7 @@ app.config.from_object(Config)
 db=SQLAlchemy(app)
 bcrypt=Bcrypt(app)
 login_manager=LoginManager(app)
-login_manager.login_view='login'
+login_manager.login_view='users.login'
 login_manager.login_message_category='info'
 
 mail=Mail(app)
@@ -18,7 +18,9 @@ mail=Mail(app)
 from flaskproject.users.routes import users
 from flaskproject.requests.routes import requests
 from flaskproject.main.routes import main
+from flaskproject.errors.handlers import errors
 
 app.register_blueprint(users)
 app.register_blueprint(requests)
 app.register_blueprint(main)
+app.register_blueprint(errors)
