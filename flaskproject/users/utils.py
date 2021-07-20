d94b842f46c9999fdc978b129c230f72c6cec81b
alonlerner <65507603+alonlerner@users.noreply.github.com>
2021-07-20 09:09:22 -0400

    added a configuration update before reseting password-07-20-2021

--- flaskproject/users/utils.py
+++ flaskproject/users/utils.py
@@ -1,9 +1,18 @@
 from flask import url_for
+from flask.globals import current_app
 from flask_mail import Message
 from flaskproject import mail
 
 def send_reset_email(user):
     token=user.get_reset_token()
+    current_app.config.update(dict(
+            MAIL_SERVER='smtp.gmail.com',
+            MAIL_PORT=465,
+            MAIL_USE_SSL=True,
+            MAIL_USERNAME='collegeemailsgenerator@gmail.com',
+            MAIL_PASSWORD='Medical2020!'
+        ))
+    mail.init_app(current_app)
     msg=Message('Password Reset Request', sender='collegeemailsgenerator@gmail.com', recipients=[user.email])
     msg.body=f'''Visit to following link to reset your password:
 {url_for('users.reset_token', token=token, _external=True)}
