2e9b82b654dfe11d9e16754fcacfbb69f01d097f
alonlerner <al663@njit.edu>
2021-09-05 15:14:56 -0400

    created the full request information page with vue

--- flaskproject/requests/routes.py
+++ flaskproject/requests/routes.py
@@ -35,6 +35,15 @@ def request_info():
 
     return render_template('request_info.html', title='New Request', teamsChoices=teamsChoices, divisions=divisions, conferences=conferences, states=states, default_subject=default_subject, default_content=default_content)
 
+@requests.route("/submit_request_info", methods=['GET', 'POST'])
+@login_required
+def submit_request_info():
+    session['subject']=request.get_json()['subject']
+    session['content']=request.get_json()['content']
+    session['teams']=request.get_json()['teams']
+    print(session['teams'])
+    return redirect(url_for('requests.review_request'))
+
 @requests.route("/request/new", methods=['GET', 'POST'])
 @login_required
 def new_request():
@@ -56,9 +65,9 @@ def review_request():
     form=ReviewRequestForm()
     teams=''
     for team in session['teams']:
-        teams = teams + Team.query.filter_by(id=team).first().name + '; '
-    subject=session['subject'].replace("[team]", Team.query.filter_by(id=team).first().name).replace("[division]", Team.query.filter_by(id=team).first().division).replace("[conference]", Team.query.filter_by(id=team).first().conference).replace("[state]", Team.query.filter_by(id=team).first().state).replace("[mascot]", Team.query.filter_by(id=team).first().mascot).replace("[coach-first-name]", Coach.query.filter_by(team_id=team).first().first_name).replace("[coach-last-name]", Coach.query.filter_by(team_id=team).first().last_name)
-    content=session['content'].replace("[team]", Team.query.filter_by(id=team).first().name).replace("[division]", Team.query.filter_by(id=team).first().division).replace("[conference]", Team.query.filter_by(id=team).first().conference).replace("[state]", Team.query.filter_by(id=team).first().state).replace("[mascot]", Team.query.filter_by(id=team).first().mascot).replace("[coach-first-name]", Coach.query.filter_by(team_id=team).first().first_name).replace("[coach-last-name]", Coach.query.filter_by(team_id=team).first().last_name)
+        teams = teams + Team.query.filter_by(id=team['id']).first().name + '; '
+    subject=session['subject'].replace("[team]", Team.query.filter_by(id=team['id']).first().name).replace("[division]", Team.query.filter_by(id=team['id']).first().division).replace("[conference]", Team.query.filter_by(id=team['id']).first().conference).replace("[state]", Team.query.filter_by(id=team['id']).first().state).replace("[mascot]", Team.query.filter_by(id=team['id']).first().mascot).replace("[coach-first-name]", Coach.query.filter_by(team_id=team['id']).first().first_name).replace("[coach-last-name]", Coach.query.filter_by(team_id=team['id']).first().last_name)
+    content=session['content'].replace("[team]", Team.query.filter_by(id=team['id']).first().name).replace("[division]", Team.query.filter_by(id=team['id']).first().division).replace("[conference]", Team.query.filter_by(id=team['id']).first().conference).replace("[state]", Team.query.filter_by(id=team['id']).first().state).replace("[mascot]", Team.query.filter_by(id=team['id']).first().mascot).replace("[coach-first-name]", Coach.query.filter_by(team_id=team['id']).first().first_name).replace("[coach-last-name]", Coach.query.filter_by(team_id=team['id']).first().last_name)
     if form.validate_on_submit():
         return redirect(url_for('requests.check_email'))
     return render_template('review_request.html', title='Review Request', form=form, subject=subject, content=content, teams=teams)
@@ -85,7 +94,7 @@ def check_email():
             emails_request=Request(email=form.email.data, subject=session['subject'], content=session['content'], sender=current_user)
             db.session.add(emails_request)
             for team in session['teams']:
-                emails_request.teams.append(Team.query.filter_by(id=team).first())
+                emails_request.teams.append(Team.query.filter_by(id=team['id']).first())
             db.session.commit()
             send_emails(form.email.data, form.password.data)
             session.pop('subject')
